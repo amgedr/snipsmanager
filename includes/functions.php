10fcc6abf44c11850c98bc36137a3d0db14c4e0b
Amged Rustom <amgadhs@codehill.com>
2012-01-19 05:35:55 +0000

    Added functions to retrieve individual parts of a snippets record. Formated comments in PHPDoc format.

--- includes/functions.php
+++ includes/functions.php
@@ -37,7 +37,9 @@
 
 include('typesfunctions.php');
 
-//Retrieves the value of the setting name passed. 
+/**
+ * Retrieves the value of the setting name passed. 
+ */
 function ch_getsetting($settingname) {
 	$result = mysql_query("SELECT * FROM settings WHERE settingname='" . $settingname . "'");
 	$setting = mysql_fetch_array($result);
@@ -50,12 +52,16 @@ function ch_getsetting($settingname) {
 	}
 }
 
-//Updates the setting called $settingname with the value passed in $settingvalue.
+/**
+ * Updates the setting called $settingname with the value passed in $settingvalue.
+ */
 function ch_savesetting($settingname, $settingvalue) {
 	mysql_query("UPDATE settings SET settingvalue='$settingvalue' WHERE settingname='$settingname'");
 }
 
-//Displays an HTML select (combobox) containing the names of social bookmark's folders, e.g. Group 1, Group 2...
+/**
+ * Displays an HTML select (combobox) containing the names of social bookmark's folders, e.g. Group 1, Group 2...
+ */
 function ch_getsocialbookmarksfolders() {
 	if ($handle = opendir('../images/sb')) {
 		$selectedValue = ch_getsetting('iconset');		
@@ -80,7 +86,9 @@ function ch_getsocialbookmarksfolders() {
 	}
 }
 
-//Displays the social bookmark icons. Each one's link is pointing to it's appropriate service.
+/**
+ * Displays the social bookmark icons. Each one's link is pointing to it's appropriate service.
+ */
 function ch_displaysocialbookmarks($currentTitle) {
 	$iconSet = 	ch_getsetting('iconset');
 	$currentUrl = urlencode($_SERVER['SERVER_NAME'] . $_SERVER["REQUEST_URI"]);
@@ -100,7 +108,9 @@ function ch_displaysocialbookmarks($currentTitle) {
 	echo $html;
 }
 
-//Displays the social bookmark icons with no links.
+/**
+ * Displays the social bookmark icons with no links.
+ */
 function ch_displaysocialbookmarksonly() {
 	$iconSet = ch_getsetting('iconset');
 	
@@ -112,7 +122,9 @@ function ch_displaysocialbookmarksonly() {
 	return $html;
 }
 
-//Displays the top menu
+/**
+ * Displays the top menu
+ */
 function ch_displaytopmenu() {
 	$top1text = ch_getsetting('topmenu1text');
 	$top1url = ch_getsetting('topmenu1url');
@@ -148,7 +160,9 @@ function ch_displaytopmenu() {
 	echo $html;
 }
 
-//Displays the logo image and sets it's title and alt tags to the title setting
+/**
+ * Displays the logo image and sets it's title and alt tags to the title setting
+ */
 function ch_displaylogo() {
 	$imageurl = ch_getsetting('logourl');
 	$title = ch_getsetting('title');
@@ -163,7 +177,9 @@ function ch_displaylogo() {
 	}
 }
 
-// clean an input
+/**
+ * Clean an input
+ */
 function clean($value)
 {  
   $value = strip_tags($value);   // strip html tags
@@ -172,14 +188,18 @@ function clean($value)
   return $value;  // return clean string
 }
 
-//Get the total number of snippets in the codes table
+/**
+ * Get the total number of snippets in the codes table
+ */
 function ch_gettotalsnippets() {
 	$result = mysql_query("SELECT COUNT(*) FROM `codes`");
 	list($total) = mysql_fetch_row($result);
 	return $total;
 }
 
-//Get a snippet by its ID
+/**
+ * Get a snippet by its ID
+ */
 function ch_getcode($id) {
 	if(empty($id))
 		return null;
@@ -195,13 +215,93 @@ function ch_getcode($id) {
 	}
 }
 
-//Decode HTML in the code for displaying on pages
+/**
+ * Decode HTML in the code for displaying on pages
+ */
 function ch_formatCodeForDisplaying($code) {
 	return html_entity_decode($code, ENT_QUOTES, "UTF-8");
 }
 
-//Encode HTML in the code for saving in database
+/**
+ * Encode HTML in the code for saving in database
+ */
 function ch_formatCodeForDatabase($code) {
 	return htmlentities($code, ENT_QUOTES, "UTF-8");
 }
+
+/**
+ * Get a snippets title by its ID
+ */
+function ch_getcodetitle($id) {
+	$result = mysql_query("SELECT `codetitle` FROM codes WHERE id = '".$id."'");
+	$row = mysql_fetch_array($result);
+		
+	if(mysql_num_rows($result)) { 
+		return $row['codetitle'];
+	}
+	else {
+		return NULL;
+	}
+}
+
+/**
+ * Checks if the supplied ID exists in codes table
+ */
+function ch_codeexists($id) {
+	$result = mysql_query("SELECT * FROM codes WHERE id = '".$id."'");
+
+	if(mysql_num_rows($result) > 0)
+		return true;
+	else
+		return false;
+}
+
+/**
+ * if the code has a password it is returned, else returns NULL
+ */
+function ch_getcodepassword($id) {
+	$result = mysql_query("SELECT `password` FROM codes WHERE id = '".$id."'");
+	$row = mysql_fetch_array($result);
+		
+	if(mysql_num_rows($result)) { 
+		return $row['password'];
+	}
+	else {
+		return NULL;
+	}
+}
+
+/**
+ * Checks if the code should ask for CAPTCHA. Returns true or false
+ */
+function ch_getcodecaptcha($id) {
+	$result = mysql_query("SELECT `captcha` FROM codes WHERE id = '".$id."'");
+	$row = mysql_fetch_array($result);
+		
+	if(mysql_num_rows($result)) { 
+		if($row['captcha'] == 0)
+			return false;
+		else
+			return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/**
+ * Returns a snippet's code type numeric ID
+ */
+function ch_getcodetype($id) {
+	$result = mysql_query("SELECT `type` FROM codes WHERE id = '".$id."'");
+	$row = mysql_fetch_array($result);
+		
+	if(mysql_num_rows($result)) { 
+		return $row['type'];
+	}
+	else {
+		return NULL;
+	}
+}
+
 ?>
