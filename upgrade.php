9e4c5aa33eba0fb45f6c1d8572234c80fdee22f8
Amged Rustom <amgadhs@codehill.com>
2012-01-29 07:29:03 +0000

    upgrade.php now adds the database changes made in version 2.2 and 2.3.

--- upgrade.php
+++ upgrade.php
@@ -35,21 +35,6 @@
  *
  */
 
-
-
-/* Upgrading to 2.3
-  
-ALTER TABLE `codes` ADD `captcha` TINYINT NULL DEFAULT '0' COMMENT 'Ask for CAPTCHA before display this snippet.'
- 
-INSERT INTO `settings` (`settingid`, `settingname`, `settingvalue`)
-VALUES (NULL, 'urlwww', '0'), (NULL, 'urlindex', '0'), (NULL, 'urlshorten', '0');
-
-  
-  
- */
-
-
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
@@ -71,64 +56,85 @@ VALUES (NULL, 'urlwww', '0'), (NULL, 'urlindex', '0'), (NULL, 'urlshorten', '0')
 	<body>
 	
 	<div style="margin:auto; margin-top:20px;width:600px; min-height:200px; background-color:white; padding:30px;">
-		<h1>SnipsManager 2.2 Upgrade</h1>
+		<h1>SnipsManager 2.3 Upgrade</h1>
 
 		<?php
         if(isset($_GET['upgrade'])) {
 			try {
 				include('config.php');
 				connect();
-				
-				//create table admin
-				mysql_query("CREATE TABLE IF NOT EXISTS `admins` (`UserName` varchar(20) NOT NULL," .
-					"`Password` varchar(32) NOT NULL, PRIMARY KEY (`UserName`)" .
-					") ENGINE=MyISAM DEFAULT CHARSET=latin1;") or 
-					die("Could not create admin table. Error: " . mysql_error());		
-			
-				//insert admin user in table admin with password admin		
-				mysql_query("INSERT INTO `admins` (`UserName`, `Password`) VALUES (" . 
-					"'admin', '21232f297a57a5a743894a0e4a801fc3');") or 
-					die("Could not insert records in table admin. Error: " . mysql_error());
+
+				//if table admins does not exist
+				if(!mysql_num_rows(mysql_query("SHOW TABLES LIKE 'admins'"))) {
+					//version 2.2
+					//create table admin
+					mysql_query("CREATE TABLE IF NOT EXISTS `admins` (`UserName` varchar(20) NOT NULL," .
+						"`Password` varchar(32) NOT NULL, PRIMARY KEY (`UserName`)" .
+						") ENGINE=MyISAM DEFAULT CHARSET=latin1;") or 
+						die("Could not create admin table. Error: " . mysql_error());		
+
+					//version 2.2
+					//insert admin user in table admin with password admin		
+					mysql_query("INSERT INTO `admins` (`UserName`, `Password`) VALUES (" . 
+						"'admin', '21232f297a57a5a743894a0e4a801fc3');") or 
+						die("Could not insert records in table admin. Error: " . mysql_error());
+				}
 		
-				//alter table codes
+				//if table settings does not exist
+				if(!mysql_num_rows(mysql_query("SHOW TABLES LIKE 'settings'"))) {
+					//version 2.2
+					//create table settings
+					mysql_query("CREATE TABLE IF NOT EXISTS `settings` (" . 
+						"`settingid` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `settingname` varchar(50) NOT NULL," .
+						" `settingvalue` varchar(300) NOT NULL, UNIQUE KEY `settingid` (`settingid`)" .
+						") ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;") or 
+						die("Could not create setting table. Error: " . mysql_error());
+
+					//version 2.2
+					//insert default settings into the settings table
+					mysql_query("INSERT INTO `settings` (`settingid`, `settingname`, `settingvalue`) VALUES " .
+						"(1, 'version', '2.2'), " .
+						"(2, 'title', 'SnipsManager'), " .
+						"(5, 'slogan', 'Share the code!'), " .
+						"(6, 'metadescription', 'A code sharing website.'), " .
+						"(17, 'iconset', '2'), " .
+						"(7, 'metakeywords', 'code sharing, text sharing'), " .
+						"(8, 'logourl', 'images/snipsmanager.png'), " .
+						"(9, 'topmenu1text', 'Home'), " .
+						"(10, 'topmenu1url', 'index.php'), " .
+						"(11, 'topmenu2text', 'Contact'), " .
+						"(12, 'topmenu2url', 'contact.php'), " .
+						"(13, 'topmenu3text', ''), " .
+						"(14, 'topmenu3url', ''), " .
+						"(15, 'topmenu4text', ''), " .
+						"(16, 'topmenu4url', ''), " .
+						"(18, 'ownername', ''), " .
+						"(19, 'owneremail', '');") 
+						or die("Could not insert records in table settings. Error: " . mysql_error());
+				}
+		
+				//version 2.2
+				//Alter table codes. Doesn't need to return an error if the SQL statement didn't execute
+				//because the only error that could occure this far in the code is Column Already Exists.
 				mysql_query("ALTER TABLE `codes` ADD COLUMN `codetitle` varchar(200) NOT NULL, " . 
-					"ADD COLUMN `submitdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP;") or 
-					die("Could not modify codes table. Error: " . mysql_error());
+					"ADD COLUMN `submitdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP;");
 		
-				//create table settings
-				mysql_query("CREATE TABLE IF NOT EXISTS `settings` (" . 
-					"`settingid` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `settingname` varchar(50) NOT NULL," .
-					" `settingvalue` varchar(300) NOT NULL, UNIQUE KEY `settingid` (`settingid`)" .
-					") ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;") or 
-					die("Could not create setting table. Error: " . mysql_error());
-		
-				//insert default settings into the settings table
-				mysql_query("INSERT INTO `settings` (`settingid`, `settingname`, `settingvalue`) VALUES " .
-					"(1, 'version', '2.2'), " .
-					"(2, 'title', 'SnipsManager'), " .
-					"(5, 'slogan', 'Share the code!'), " .
-					"(6, 'metadescription', 'A code sharing website.'), " .
-					"(17, 'iconset', '2'), " .
-					"(7, 'metakeywords', 'code sharing, text sharing'), " .
-					"(8, 'logourl', 'images/snipsmanager.png'), " .
-					"(9, 'topmenu1text', 'Home'), " .
-					"(10, 'topmenu1url', 'index.php'), " .
-					"(11, 'topmenu2text', 'Contact'), " .
-					"(12, 'topmenu2url', 'contact.php'), " .
-					"(13, 'topmenu3text', ''), " .
-					"(14, 'topmenu3url', ''), " .
-					"(15, 'topmenu4text', ''), " .
-					"(16, 'topmenu4url', ''), " .
-					"(18, 'ownername', ''), " .
-					"(19, 'owneremail', '');") 
-					or die("Could not insert records in table settings. Error: " . mysql_error());
-		
+				//version 2.3
+				//Alter table codes. Doesn't need to return an error if the SQL statement didn't execute
+				//because the only error that could occure this far in the code is Column Already Exists.
+				mysql_query("ALTER TABLE `codes` ADD `captcha` TINYINT NULL DEFAULT '0' COMMENT " .
+					"'Ask for CAPTCHA before display this snippet.'");
+
+				//version 2.3
+				mysql_query("INSERT INTO `settings` (`settingid`, `settingname`, `settingvalue`) " .
+					"VALUES (NULL, 'urlwww', '0'), (NULL, 'urlindex', '0'), (NULL, 'urlshorten', '0')"); 
 				
 				echo '<p style="color:green; font-weight:bold;">The database upgrade completed successfully.</p>';
 			
 			} catch (Exception $e) {
 				echo '<p style="color:red; font-weight:bold;">' . $e->getMessage() . '</p>';
 			}
+			
 		?>
 
 		<br /><br />
