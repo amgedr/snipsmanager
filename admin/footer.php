37d1b119aeb181cc9603cf664680aeab5438be04
Amged Rustom <amgadhs@codehill.com>
2013-12-19 15:40:55 +0400

    Formatted footer code.

--- admin/footer.php
+++ admin/footer.php
@@ -1,5 +1,11 @@
 </div>
-<div class='content_bot'></div><center><span style="color: #FFFFFF; font-size:10px;">Powered by: <strong><a href="http://www.snipsmanager.com">SnipsManager <?php echo ch_getsetting("version"); ?></a></strong></span></center><?php //please do NOT remove these lines ?><br />
+<div class='content_bot'></div>
+	<center>
+		<span style="color: #FFFFFF; font-size:11px;line-height:15px;">
+			<p>Thank you for using <a href="http://www.snipsmanager.com">SnipsManager <?php echo ch_getsetting("version"); ?></a></p>
+		</span>
+	</center>
+	<br />
 </div>
 </body>
 </html>
