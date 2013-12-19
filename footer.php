7070353440a5117b9d1178b538bff547c280d371
Amged Rustom <amgadhs@codehill.com>
2013-12-19 15:32:35 +0400

    Removed version number from footer.

--- footer.php
+++ footer.php
@@ -3,7 +3,12 @@
 
    </div>
 </div>
-<div class='content_bot'></div><center><span style="color: #FFFFFF; font-size:10px;">Powered by: <strong><a href="http://www.snipsmanager.com">SnipsManager <?php echo ch_getsetting("version"); ?></a></strong></span></center><?php //please do NOT remove these lines ?>
+<div class='content_bot'></div>
+	<center>
+		<span style="color: #FFFFFF; font-size:10px;">
+			Powered by:	<strong><a href="http://www.snipsmanager.com">SnipsManager</a></strong>
+		</span>
+	</center>
 </div>
 </div>
 </body>
